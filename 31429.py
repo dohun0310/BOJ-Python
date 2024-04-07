2a2e39cebf03ce439891ea94f21506fe971b3859
dohun0310 <dohun0310@naver.com>
2024-04-07 19:45:11 +0900

    solve 31429

---
+++ 31429.py
@@ -0,0 +1,23 @@
+i = int(input())
+if i == 1:
+    print(12, 1600)
+elif i == 2:
+    print(11, 894)
+elif i == 3:
+    print(11, 1327)
+elif i == 4:
+    print(10, 1311)
+elif i == 5:
+    print(9, 1004)
+elif i == 6:
+    print(9, 1178)
+elif i == 7:
+    print(9, 1357)
+elif i == 8:
+    print(8, 837)
+elif i == 9:
+    print(7, 1055)
+elif i == 10:
+    print(6, 556)
+else:
+    print(6, 773)
