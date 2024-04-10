85122086d27e293b3d231c1f1d6ef9bf568a8a7e
dohun0310 <dohun0310@naver.com>
2024-04-10 14:08:06 +0900

    solve 2798

--- 2798.py
+++ 2798.py
@@ -1,9 +1,9 @@
 n, m = map(int, input().split())
 l = list(map(int, input().split()))
-s = []
+s = 0
 for i in range(n - 2):
-    for j in range(i + 1, n -1):
-        for k in range(i + 2, n):
+    for j in range(i + 1, n - 1):
+        for k in range(j + 1, n):
             if l[i] + l[j] + l[k] <= m:
-                s.append(l[i] + l[j] + l[k])
-print(max(s))
+                s = max(s, l[i] + l[j] + l[k])
+print(s)
