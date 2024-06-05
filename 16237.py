690f880ab5195a1fb32c8c6a24c7271e89ca6a4f
dohun0310 <dohun0310@naver.com>
2024-06-05 11:07:32 +0900

    solve 16237

---
+++ 16237.py
@@ -0,0 +1,27 @@
+a, b, c, d, e = map(int, input().split())
+s = e + d
+a -= d
+while c > 0:
+    s += 1
+    if b > 0:
+        b -= 1
+        c -= 1
+    elif a > 0:
+        a -= 2
+        c -= 1
+    else:
+        c -= 1
+while b > 0:
+    s += 1
+    if b > 1 and a > 0:
+        b -= 2
+        a -= 1
+    elif b > 0 and a > 0:
+        b -= 1
+        a -= 3
+    else:
+        b -= 2
+while a > 0:
+    s += 1
+    a -= 5
+print(s)
