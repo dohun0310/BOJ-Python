4aa2fd146fc097b329fe1d9da4250d5e3ac3c918
김도훈 <dohun0310@naver.com>
2025-09-09 00:55:34 +0900

    solve 2864

--- 2864.py
+++ 2864.py
@@ -1,4 +1,2 @@
 a, b = input().split()
-m = int(a.replace("5", "6")) + int(b.replace("5", "6"))
-n = int(a.replace("6", "5")) + int(b.replace("6", "5"))
-print(n, m)
+print(int(a.replace("6", "5")) + int(b.replace("6", "5")), int(a.replace("5", "6")) + int(b.replace("5", "6")))
