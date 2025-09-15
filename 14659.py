97082e398500e6afde463d80623fec204d3936ee
김도훈 <dohun0310@naver.com>
2025-09-15 11:47:17 +0900

    solve 14659

--- 14659.py
+++ 14659.py
@@ -1,12 +1,12 @@
 n = int(input())
 l = list(map(int, input().split()))
-s, c, m = 0, 0, 0
-for i in l:
-    if s < i:
-        c = 0
-        s = i
-    else:
-        c += 1
-        if m < c:
-            m = c
-print(m)
+p = 0
+for i in range(n):
+    c = 0
+    for j in range(i + 1, n):
+        if l[i] > l[j]:
+            c += 1
+        else:
+            break
+    p = max(p, c)
+print(p)
